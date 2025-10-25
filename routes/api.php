b5015742627cc193f6ae71b611e1b57aefdfd8fc
Roberto0611 <ochoacuevasr@gmail.com>
2025-10-25 08:30:28 -0600

    cambie apis de lugar

--- routes/api.php
+++ routes/api.php
@@ -14,16 +14,15 @@ Route::post('login', [JWTAuthController::class, 'login']);
 Route::middleware('auth:api')->group(function () {
     Route::get('user', [JWTAuthController::class, 'me']);
     Route::post('logout', [JWTAuthController::class, 'logout']);
-    Route::get('getDiscounts', [DiscountsController::class, 'index']);
-    
-    // rutas places
-    Route::get('getPlaces', [placesController::class, 'index']);
-    
-    // rutas products
-    Route::get('getProducts', [ProductsController::class, 'index']);
-    
     // rutas 
-        // make a verify token route
-        Route::post('verify-token', [JWTAuthController::class, 'verifyToken']);
+    // make a verify token route
+     Route::post('verify-token', [JWTAuthController::class, 'verifyToken']);
 });
 
+Route::get('getDiscounts', [DiscountsController::class, 'index']);
+    
+// rutas places
+Route::get('getPlaces', [placesController::class, 'index']);
+    
+// rutas products
+Route::get('getProducts', [ProductsController::class, 'index']);
